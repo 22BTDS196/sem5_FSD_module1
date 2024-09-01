0186236d64ed930a6ce998683f66bc25362ab6ac
Gouthami C <gouthamic2004@gmail.com>
2024-09-01 15:21:27 +0530

    .

---
+++ array.js
@@ -0,0 +1,13 @@
+var students = [ "Raj", "Vinay", "Sam"]
+console.log(students.length)
+students.pop("Karuna") /*Will remove object from end--pop. Karuna doesnt matter*/
+console.log(students)
+students.push("Karuna") /*Will add at end--push */
+/*Will remove object from start--shift */
+/*unshift-- add at beginning */
+console.log(students)
+
+
+var a= [1,2,3,4,5,6,7]
+var b= a.slice(1,2)
+console.log (b)
